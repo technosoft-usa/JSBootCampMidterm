f57e5efe2a582bbeb377a06775e707a428ce785e
aumialam <aumialam@gmail.com>
2022-07-01 21:35:53 -0400

    Midterm

--- tests/00_expo/expo.js
+++ tests/00_expo/expo.js
@@ -1,6 +1,9 @@
 // Do not use Math.pow() in your solution.
-function expo() {
-
+function expo(num, exponent) {
+  let result = 1;
+  for (let i = 1; i <= exponent; i++) {
+    result *= num;
+  }
+  return result;
 }
-
 module.exports = { expo };
--- tests/03_roundToNext5/roundToNext5.js
+++ tests/03_roundToNext5/roundToNext5.js
@@ -1,5 +1,10 @@
-function roundToNext5() {
-
+function roundToNext5(num) {
+  if (num % 5 === 0) {
+    return num;
+  } else if (num % 5 < 1) {
+    return num - (num % 5);
+  }
+  return num + (5 - (num % 5));
 }
 
 module.exports = { roundToNext5 };
